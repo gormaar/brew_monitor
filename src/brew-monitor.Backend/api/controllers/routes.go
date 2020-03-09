6cfd6fec39bbc27e21c8a11824e67ec209b2e5f7
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2020-03-09 20:39:55 +0100

    Added DB env and configured routes

--- src/brew-monitor.Backend/api/controllers/routes.go
+++ src/brew-monitor.Backend/api/controllers/routes.go
@@ -1,13 +1,27 @@
 package controllers
 
-import (
-	controller "./"
-)
+
 
 func (server *Server) initializeRoutes() {
-	s.router.HandleFunc("/api/respiration", controller.GetRecentRespirationData).Methods("GET")
-	s.router.HandleFunc("/api/hourlyRespiration", controller.GetHourlyRespirationData).Methods("GET")
-	s.router.HandleFunc("/api/respiration", controller.CreateRespiration).Methods("POST")
-	s.router.HandleFunc("api/respiration", controller.PutRespiration).Methods("PUT")
-	s.router.HandleFunc("api/respiration", controller.DeleteRespiration).Methods("DELETE")
+
+	//Routes for respiration
+	server.Router.HandleFunc("/api/respiration", GetRecentRespirationData).Methods("GET")
+	server.Router.HandleFunc("/api/hourlyRespiration", GetHourlyRespirationData).Methods("GET")
+	server.Router.HandleFunc("/api/respiration", CreateRespiration).Methods("POST")
+	server.Router.HandleFunc("api/respiration", PutRespiration).Methods("PUT")
+	server.Router.HandleFunc("api/respiration", DeleteRespiration).Methods("DELETE")
+
+	//Routes for temperature
+	server.Router.HandleFunc("/api/respiration", GetRecentTemperatureData).Methods("GET")
+	server.Router.HandleFunc("/api/hourlyRespiration", GetHourlyTemperatureData).Methods("GET")
+	server.Router.HandleFunc("/api/respiration", CreateTemperature).Methods("POST")
+	server.Router.HandleFunc("api/respiration", PutTemperature).Methods("PUT")
+	server.Router.HandleFunc("api/respiration", DeleteTemperature).Methods("DELETE")
+
+	//Routes for brews
+	server.Router.HandleFunc("/api/respiration", GetSingleBrew).Methods("GET")
+	server.Router.HandleFunc("/api/hourlyRespiration", GetAllBrews).Methods("GET")
+	server.Router.HandleFunc("/api/respiration", CreateBrew).Methods("POST")
+	server.Router.HandleFunc("api/respiration", PutBrew).Methods("PUT")
+	server.Router.HandleFunc("api/respiration", DeleteBrew).Methods("DELETE")
 }
