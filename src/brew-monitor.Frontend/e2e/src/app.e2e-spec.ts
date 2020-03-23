ac8b13b18b32954edae55cff4d76520ec03c0df9
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2020-03-23 20:11:30 +0100

    Rebuilt frontend with sass

--- src/brew-monitor.Frontend/e2e/src/app.e2e-spec.ts
+++ src/brew-monitor.Frontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('brew-monitor app is running!');
+    expect(page.getTitleText()).toEqual('brew-monitor-frontend app is running!');
   });
 
   afterEach(async () => {
