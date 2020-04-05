a104ab8a46ac8998c9d5051421e525fef2030158
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2020-04-05 19:45:36 +0200

    Rebuilt frontend with scss

--- src/brew-monitor.Frontend/src/app/app.module.ts
+++ src/brew-monitor.Frontend/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { BrewListComponent } from './components/brew-list/brew-list.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BrewListComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
