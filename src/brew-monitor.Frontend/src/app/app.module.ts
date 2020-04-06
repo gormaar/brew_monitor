ac7969a2eb6c827da4bd5dadf1fd8c0356ad527a
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2020-04-06 21:41:52 +0200

    Added statistic and brew components

--- src/brew-monitor.Frontend/src/app/app.module.ts
+++ src/brew-monitor.Frontend/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrewListComponent } from './components/brew-list/brew-list.component';
 import { MainComponent } from './components/main/main.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { BrewComponent } from './components/brew/brew.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     BrewListComponent,
-    MainComponent
+    MainComponent,
+    StatisticsComponent,
+    BrewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/brew-monitor.Frontend/src/app/components/statistics/statistics.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+import { Chart } from "chart.js";
+
+@Component({
+  selector: "app-statistics",
+  templateUrl: "./statistics.component.html",
+  styleUrls: ["./statistics.component.scss"],
+})
+export class StatisticsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
