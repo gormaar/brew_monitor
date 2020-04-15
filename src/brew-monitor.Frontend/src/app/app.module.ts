837df48eadef331807f57202d2a029eaacb8fcfe
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2020-04-15 22:53:52 +0200

    Added and implemented status component

--- src/brew-monitor.Frontend/src/app/app.module.ts
+++ src/brew-monitor.Frontend/src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { BrewListComponent } from './components/brew-list/brew-list.component';
-import { MainComponent } from './components/main/main.component';
-import { StatisticsComponent } from './components/statistics/statistics.component';
-import { BrewComponent } from './components/brew/brew.component';
-import { RespirationComponent } from './components/respiration/respiration.component';
-import { TemperatureComponent } from './components/temperature/temperature.component';
-import { BrewListItemComponent } from './components/brew-list-item/brew-list-item.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
+import { BrewListComponent } from "./components/brew-list/brew-list.component";
+import { MainComponent } from "./components/main/main.component";
+import { StatisticsComponent } from "./components/statistics/statistics.component";
+import { BrewComponent } from "./components/brew/brew.component";
+import { RespirationComponent } from "./components/respiration/respiration.component";
+import { TemperatureComponent } from "./components/temperature/temperature.component";
+import { BrewListItemComponent } from "./components/brew-list-item/brew-list-item.component";
+import { StatusComponent } from "./components/status/status.component";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -22,13 +25,11 @@ import { BrewListItemComponent } from './components/brew-list-item/brew-list-ite
     BrewComponent,
     RespirationComponent,
     TemperatureComponent,
-    BrewListItemComponent
+    BrewListItemComponent,
+    StatusComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, MatProgressBarModule, NoopAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
