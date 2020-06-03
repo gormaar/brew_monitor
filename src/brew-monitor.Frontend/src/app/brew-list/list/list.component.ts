455b1838a76584bba744eeee634fe872cc0e0a8d
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2020-06-03 23:23:55 +0200

    Implemented conditional rendering on brew-form

--- src/brew-monitor.Frontend/src/app/brew-list/list/list.component.ts
+++ src/brew-monitor.Frontend/src/app/brew-list/list/list.component.ts
@@ -9,14 +9,20 @@ import { BrewService } from "../../shared/services/brew/brew.service";
 })
 export class ListComponent implements OnInit {
   brews: IBrewModel[];
+  displayForm: boolean;
 
   constructor(private _brewService: BrewService) {}
 
   ngOnInit(): void {
     this.brews = this._brewService.getBrews();
+    this.displayForm = false;
   }
 
   selectBrew(brew: IBrewModel): void {
     this._brewService.setBrew(brew);
+  }
+
+  toggleForm() {
+    this._brewService.toggleForm();
   }
 }
--- src/brew-monitor.Frontend/src/app/shared/services/brew/brew.service.ts
+++ src/brew-monitor.Frontend/src/app/shared/services/brew/brew.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable, BehaviorSubject } from "rxjs";
+import { Observable, BehaviorSubject, observable } from "rxjs";
 
 export interface IBrewModel {
   id: number;
@@ -17,11 +17,15 @@ export class BrewService {
   activeBrew: BehaviorSubject<IBrewModel>;
   brews: IBrewModel[];
   activeBrewStream: Observable<IBrewModel>;
+  displayForm: BehaviorSubject<boolean>;
+  displayFormStream: Observable<boolean>;
 
   constructor(private _http: HttpClient) {
     this.brews = this.getBrews();
     this.activeBrew = new BehaviorSubject<IBrewModel>(this.brews[0]);
     this.activeBrewStream = this.activeBrew.asObservable();
+    this.displayForm = new BehaviorSubject<boolean>(false);
+    this.displayFormStream = this.displayForm.asObservable();
   }
 
   getBrew(id: number): Observable<IBrewModel> {
@@ -64,5 +68,9 @@ export class BrewService {
 
   setBrew(brew: IBrewModel) {
     this.activeBrew.next(brew);
+  }
+
+  toggleForm() {
+    this.displayForm.next(!this.displayForm.value);
   }
 }
