7989a29bbdf67c7c2172125ca9a503d20772c947
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2020-06-06 19:15:59 +0200

    Fixed form structure

--- src/brew-monitor.Frontend/src/app/brew-list/brew-form/brew-form.component.ts
+++ src/brew-monitor.Frontend/src/app/brew-list/brew-form/brew-form.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./brew-form.component.scss"],
 })
 export class BrewFormComponent implements OnInit {
+  brewInputs: String[];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.brewInputs = [
+      "Brew name",
+      "Brew type",
+      "Brew date",
+      "Fermentation start-date",
+      "Fermentation end-date",
+      "Original Gravity",
+      "Specific Gravity",
+      "Final Gravity",
+      "Optimal fermentation temperature",
+      "Fermentation temperature threshold",
+      "Bottled days",
+      "Water",
+      "Barley",
+      "Hops",
+      "Extra",
+    ];
+  }
 }
