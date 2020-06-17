3db993aadcbf64361771a8228dcceeda935ea4cc
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2020-06-18 00:12:50 +0200

    Started refactor of brew-form

--- src/brew-monitor.Frontend/src/app/brew-list/brew-form/brew-form.component.ts
+++ src/brew-monitor.Frontend/src/app/brew-list/brew-form/brew-form.component.ts
@@ -2,20 +2,16 @@ import { Component, OnInit } from "@angular/core";
 import { BrewService } from "src/app/shared/services/brew/brew.service";
 import { FormBuilder, FormGroup } from "@angular/forms";
 
-interface IInputTags {
-  details: String[];
-  fermentation: String[];
-  ingredients: String[];
-}
-
 @Component({
   selector: "brew-form",
   templateUrl: "./brew-form.component.html",
   styleUrls: ["./brew-form.component.scss"],
 })
 export class BrewFormComponent implements OnInit {
-  inputTags: IInputTags;
-  brewForm: FormGroup;
+  inputTags: String[];
+  detailsForm: FormGroup;
+  fermentForm: FormGroup;
+  ingredientsForm: FormGroup;
 
   constructor(
     private _brewService: BrewService,
@@ -23,8 +19,8 @@ export class BrewFormComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.brewForm = this._formBuilder.group({
-      brewName: "",
+    this.detailsForm = this._formBuilder.group({
+      Brewname: "",
       brewType: "",
       brewDate: "",
       mashingTemp: "",
@@ -33,27 +29,48 @@ export class BrewFormComponent implements OnInit {
       amount: "",
     });
 
-    this.inputTags = {
-      details: [
-        "Brew name",
-        "Mashing temperature",
-        "Brew date",
-        "Mashing duration",
-        "Brew type",
-        "Total amount",
-      ],
-      fermentation: [
-        "Fermentation start-date",
-        "Original Gravity",
-        "Fermentation end-date",
-        "Specific Gravity",
-        "Fermentation temperature",
-        "Final Gravity",
-        "Fermentation temperature threshold",
-        "Bottled days",
-      ],
-      ingredients: ["Barley", "Hops", "Yeast", "Extra", "Water"],
-    };
+    this.fermentForm = this._formBuilder.group({
+      startDate: "",
+      endDate: "",
+      temperature: "",
+      tempThreshold: "",
+      OG: "",
+      SG: "",
+      FG: "",
+      daysBottle: "",
+    });
+
+    this.ingredientsForm = this._formBuilder.group({
+      barley: "",
+      yeast: "",
+      hops: "",
+      water: "",
+      extra: "",
+    });
+
+    this.inputTags = [
+      "Brew name",
+      "Mashing temperature",
+      "Brew date",
+      "Mashing duration",
+      "Brew type",
+      "Total amount",
+
+      "Fermentation start-date",
+      "Original Gravity",
+      "Fermentation end-date",
+      "Specific Gravity",
+      "Fermentation temperature",
+      "Final Gravity",
+      "Fermentation temperature threshold",
+      "Bottled days",
+
+      "Barley",
+      "Hops",
+      "Yeast",
+      "Extra",
+      "Water",
+    ];
   }
 
   closeForm() {
--- src/brew-monitor.Frontend/src/app/brew-list/brew-list.module.ts
+++ src/brew-monitor.Frontend/src/app/brew-list/brew-list.module.ts
@@ -1,14 +1,15 @@
+import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+
 import { CommonModule } from "@angular/common";
 import { ListComponent } from "./list/list.component";
 import { ListItemComponent } from "./list-item/list-item.component";
 import { AppRoutingModule } from "../app-routing.module";
 import { SharedModule } from "../shared/shared.module";
 import { BrewFormComponent } from "./brew-form/brew-form.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrewService } from "../shared/services/brew/brew.service";
 import { ReactiveFormsModule } from "@angular/forms";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { BrowserModule } from "@angular/platform-browser";
 
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
@@ -18,16 +19,16 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 @NgModule({
   declarations: [ListComponent, ListItemComponent, BrewFormComponent],
   imports: [
+    BrowserModule,
     CommonModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     SharedModule,
     MatIconModule,
     MatInputModule,
     MatButtonModule,
     MatFormFieldModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule,
-    BrowserModule,
   ],
   exports: [ListComponent, BrewFormComponent],
   providers: [BrewService],
