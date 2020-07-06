c951d29131c73b3213d5f618ffec4db4c2f7efaf
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2020-07-06 22:05:09 +0200

    Added new components to project

---
+++ src/brew-monitor.frontend-react/src/components/containers/Details/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const Details: React.FC = () => {
+	return (
+		<div className={styles.container}>
+			<p>Details is working!</p>
+		</div>
+	);
+};
+
+export default Details;
