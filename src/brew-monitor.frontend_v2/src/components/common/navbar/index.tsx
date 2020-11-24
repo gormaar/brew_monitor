d442b0c75bbf8575bbf7616c440b68902ad3e117
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2020-11-24 22:27:56 +0100

    Moved BrewSelector to navbar

--- src/brew-monitor.frontend_v2/src/components/common/navbar/index.tsx
+++ src/brew-monitor.frontend_v2/src/components/common/navbar/index.tsx
@@ -1,15 +1,16 @@
 import React, { FC, Fragment } from 'react';
 import Appbar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Select from '../select';
 
-const Navbar: FC = () => {
+type NavbarProps = {
+  children?: React.ReactNode | React.ReactNode[];
+};
+
+const Navbar: FC<NavbarProps> = ({ children }) => {
   return (
     <Fragment>
       <Appbar style={{ background: '#808080' }} position="sticky">
-        <Toolbar>
-          <Select />
-        </Toolbar>
+        <Toolbar>{children}</Toolbar>
       </Appbar>
     </Fragment>
   );
--- src/brew-monitor.frontend_v2/src/screens/dashboardScreen/index.tsx
+++ src/brew-monitor.frontend_v2/src/screens/dashboardScreen/index.tsx
@@ -4,12 +4,15 @@ import Navbar from '../../components/common/navbar';
 import Box from '@material-ui/core/Box';
 import Respirator from '../../components/statistics/components/respirator';
 import Typography from '@material-ui/core/Typography';
+import BrewSelector from '../../components/common/select';
 import './styles.scss';
 
 const DashboardScreen: FC = () => {
   return (
     <Fragment>
-      <Navbar />
+      <Navbar>
+        <BrewSelector />
+      </Navbar>
       <Box className="dashboard">
         <Typography variant="h3" gutterBottom>
           Name placeholder
