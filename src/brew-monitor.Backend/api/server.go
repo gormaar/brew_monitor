78bc3335a39d681551ac8d57df39ae5a28ea9aee
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2021-01-18 22:41:15 +0100

    Added build-args to docker build pipeline

--- src/brew-monitor.Backend/api/server.go
+++ src/brew-monitor.Backend/api/server.go
@@ -18,7 +18,7 @@ func Run() {
 		log.Fatalf("Error when loading .env file %v", err)
 	}
 
-	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST") , os.Getenv("DB_NAME"))
+	server.Initialize("mysql", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), "3306", "127.0.0.1" , os.Getenv("DB_NAME"))
 
 	server.Run()
 }
