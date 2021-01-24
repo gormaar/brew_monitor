8c5f6a22616caaac0a95b57bf5665d5c3ceacd43
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2021-01-24 22:59:47 +0100

    Added env to godotenv

--- src/brew-monitor.Backend/api/server.go
+++ src/brew-monitor.Backend/api/server.go
@@ -15,10 +15,10 @@ func Run() {
 	var err error
 	err = godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error when loading .env file %v", err)
+		log.Printf("Error when loading .env file %v", err)
 	}
 
-	server.Initialize("mysql", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), "3306", "127.0.0.1" , os.Getenv("DB_NAME"))
+	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST") , os.Getenv("DB_NAME"))
 
 	server.Run()
 }
