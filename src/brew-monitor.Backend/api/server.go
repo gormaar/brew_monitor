82ce9b962acfea8e2e0b89c282facef1ef77f9e4
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2021-02-07 17:55:22 +0100

    Server config

--- src/brew-monitor.Backend/api/server.go
+++ src/brew-monitor.Backend/api/server.go
@@ -5,18 +5,18 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/joho/godotenv"
-	"log"
+	//"log"
 	"os"
 )
 
 var server = controllers.Server{}
 
 func Run() {
-	var err error
-	err = godotenv.Load()
-	if err != nil {
-		log.Printf("Error when loading .env file %v", err)
-	}
+	//var err error
+	godotenv.Load()
+	// if err != nil {
+	// 	log.Printf("Error when loading .env file %v", err)
+	// }
 
 	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST") , os.Getenv("DB_NAME"))
 
