3b1237e609f73f0bc0bd2115583791e76b79f92c
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2021-04-11 12:22:59 +0200

    env

--- src/brew-monitor.Backend/api/server.go
+++ src/brew-monitor.Backend/api/server.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/joho/godotenv"
+	"os"
 	//"log"
 
 )
@@ -18,7 +19,7 @@ func Run() {
 	// 	log.Printf("Error when loading .env file %v", err)
 	// }
 
-	//server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST") , os.Getenv("DB_NAME"))
-	server.Initialize("mysql", "gormerik", "Cyberpunk2077!", "3306", "127.0.0.1" , "brew_monitor")
+	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST") , os.Getenv("DB_NAME"))
+	
 	server.Run()
 }
