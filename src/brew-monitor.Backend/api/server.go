26f921d00ae7482c94b337c01c517f8da0e68f75
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2021-04-13 21:53:01 +0200

    Fixed Dockerfiles for build

--- src/brew-monitor.Backend/api/server.go
+++ src/brew-monitor.Backend/api/server.go
@@ -7,18 +7,12 @@ import (
 	"github.com/gormaar/brew-monitor/api/controllers"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/joho/godotenv"
-	//"log"
 )
 
 var server = controllers.Server{}
 
 func Run() {
-	//var err error
 	godotenv.Load()
-	// if err != nil {
-	// 	log.Printf("Error when loading .env file %v", err)
-	// }
-
 	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST") , os.Getenv("DB_NAME"))
 	server.Run()
 }
