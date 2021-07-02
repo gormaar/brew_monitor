7caf194423178c100e239f21020d0ab474cb17db
Gorm-Erik Aarsheim <35703118+gormaar@users.noreply.github.com>
2021-07-03 01:24:59 +0200

    Update server.go

--- src/brew-monitor.Backend/api/server.go
+++ src/brew-monitor.Backend/api/server.go
@@ -9,14 +9,9 @@ import (
 
 var server = controllers.Server{}
 
-// func Run() {
-// 	godotenv.Load()
-// 	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST") , os.Getenv("DB_NAME"))
-// 	server.Run()
-// }
-
 func Run() {
 	godotenv.Load()
-	server.Initialize("mysql", "gormerik", "Cyberpunk2077!", "3306", "127.0.0.1", "brew_monitor")
-	server.Run()
-}
+ 	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST") , os.Getenv("DB_NAME"))
+ 	server.Run()
+ }
+
