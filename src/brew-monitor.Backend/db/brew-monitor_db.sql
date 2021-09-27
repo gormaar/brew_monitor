5be3638158e5db5dddc74dfe7789a173ad7bca18
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2021-09-27 23:01:36 +0200

    Updated mysql create script

--- src/brew-monitor.Backend/db/brew-monitor_db.sql
+++ src/brew-monitor.Backend/db/brew-monitor_db.sql
@@ -5,8 +5,10 @@ create table if not exists Brews(
 brew_id integer not null auto_increment,
 brew_name varchar(100),
 brew_type varchar(100),
-created_date datetime,
-modified_date datetime,
+brew_status varchar(100),
+brew_fermentation_time integer,
+created_at datetime,
+updated_at datetime,
 
 constraint brew_pk primary key (brew_id)
 );
@@ -14,19 +16,20 @@ constraint brew_pk primary key (brew_id)
 create table if not exists Temperature(
 temp_id integer not null auto_increment,
 temp_value decimal,
-created_date datetime,
-modified_date datetime,
+created_at datetime,
+updated_at datetime,
 brew_id integer,
 
 constraint temp_pk primary key (temp_id),
 constraint temp_fk_1 foreign key (brew_id) references Brews (brew_id)
 );
 
-create table if not exists Airlock(
+create table if not exists Airlocks(
 airlock_id integer not null auto_increment,
 airlock_activity integer,
-created_date datetime,
-modified_date datetime,
+airlock_activity_longterm integer,
+created_at datetime,
+updated_at datetime,
 brew_id integer,
 
 constraint airlock_pk primary key (airlock_id),
@@ -35,6 +38,8 @@ constraint airlock_fk_1 foreign key (brew_id) references Brews (brew_id)
 
 create table if not exists Ingredients(
 ingredients_id integer not null auto_increment,
+created_at datetime,
+updated_at datetime,
 brew_id integer,
 
 constraint ingredients_pk primary key (ingredients_id),
@@ -45,6 +50,8 @@ create table if not exists Hops(
 hop_id integer not null auto_increment,
 hop_type varchar(100),
 hop_amount integer,
+created_at datetime,
+updated_at datetime,
 ingredients_id integer,
 
 constraint hop_pk primary key (hop_id),
@@ -55,6 +62,8 @@ create table if not exists Yeast(
 yeast_id integer not null auto_increment,
 yeast_type varchar(100),
 yeast_amount integer,
+created_at datetime,
+updated_at datetime,
 ingredients_id integer,
 
 constraint yeast_pk primary key (yeast_id),
@@ -65,6 +74,8 @@ create table if not exists Barley(
 barley_id integer not null auto_increment,
 barley_type varchar(100),
 barley_amount integer,
+created_at datetime,
+updated_at datetime,
 ingredients_id integer,
 
 constraint barley_pk primary key (barley_id),
