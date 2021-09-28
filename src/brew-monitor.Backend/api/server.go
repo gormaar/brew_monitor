4f481af292528d40a2f906001fc1e6526264128e
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2021-09-28 08:09:06 +0200

    Changed db user

--- src/brew-monitor.Backend/api/server.go
+++ src/brew-monitor.Backend/api/server.go
@@ -13,7 +13,7 @@ var server = controllers.Server{}
 
 func Run() {
 	godotenv.Load()
- 	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST") , os.Getenv("DB_NAME"))
+ 	server.Initialize(os.Getenv("DB_DRIVER"), "root", os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST") , os.Getenv("DB_NAME"))
  	server.Run()
  }
 
