bdadb9165ddd3b1e797589ebd5ed72137a97ca7c
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2022-01-05 15:41:35 +0100

    Changed paths in reverse proxy

--- src/brew-monitor.frontend/src/constants.ts
+++ src/brew-monitor.frontend/src/constants.ts
@@ -1,9 +1,9 @@
 export const getBackendBaseUri = () => {
-    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-        return 'http://localhost:8080'
-    } else if (process.env.REACT_APP_API_URL && process.env.NODE_ENV === 'production') {
-        return process.env.REACT_APP_API_URL
-    } else {
-        return 'https://www.brew-monitor.no/api'
-    }
-}
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    return 'http://localhost:8080';
+  } else if (process.env.REACT_APP_API_URL && process.env.NODE_ENV === 'production') {
+    return process.env.REACT_APP_API_URL;
+  } else {
+    return 'https://www.gormaar.no/brew-monitor/api';
+  }
+};
